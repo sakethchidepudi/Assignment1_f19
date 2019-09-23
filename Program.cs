bac9e39ad394015a2240e21547dd91f1f44c4f28
sakethchidepudi <chidepudis@mail.usf.edu>
2019-09-23 03:28:03 -0400

    Comments

--- Program.cs
+++ Program.cs
@@ -37,25 +37,25 @@ namespace Assignment1_F19
             {
                 if (x > y)
                 {
-                    Console.WriteLine("Invalid input");
+                    Console.WriteLine("Invalid input");//If the first number is greater than second number,this part gets exeuted
                 }
                 else
                 {
-                    for (int num = x; num <= y; num++)
+                    for (int num = x; num <= y; num++)//This loop runs from the first number through the last number of series
                     {
                         int SelfDividing = 1;
-                        for (int temp = num; temp > 0; temp /= 10)
+                        for (int temp = num; temp > 0; temp /= 10)//taking each number and dividing with 10 and saving it for next iteration
                         {
                             int dig = temp % 10;
-                            if (dig == 0 || num % dig != 0)
+                            if (dig == 0 || num % dig != 0)//getting the modulus and check if it can divide our number
                             {
                                 SelfDividing = 0;
                                 break;
                             }
                         }
-                        if (SelfDividing == 1)
+                        if (SelfDividing == 1)// If the number is selfdividing it comes here and gets into the output series
                         {
-                            Console.Write("{0},",num);
+                            Console.Write(" {0},",num);//Desired output 
                         }
                     }
                 }
@@ -70,20 +70,20 @@ namespace Assignment1_F19
         {
             try
             {
-                int print_count = 0;
+                int print_count = 0;//initiating two variables in order to keep a count while getting the desired output
                 int start_count = 1;
                 do
                 {
-                    for (int i = 0; i < start_count; i++)
+                    for (int i = 0; i < start_count; i++)  //loop for printing the series
                     {
                         Console.Write(start_count);
                         print_count += 1;
-                        if (print_count == n)
+                        if (print_count == n)//If my variable reaches desired user input value it will break out.
                         {
                             break;
                         }
                     }
-                    start_count += 1;
+                    start_count += 1; //Incrementing my variable in first loop so that the number n gets displayed n times
                 } while (print_count < n);
             }
             catch
@@ -96,15 +96,15 @@ namespace Assignment1_F19
         {
             try
             {
-                for (int i = n; i >= 1; i--)
+                for (int i = n; i >= 1; i--)// To run the loop n times
                 {
-                    for (int k = 0; k < n - i; k++)
+                    for (int k = 0; k < n - i; k++) //Loop for printing the spaces as rows get executed
                         Console.Write(" ");
-                    for (int j = i; j <= (2 * i) - 1; j++)
+                    for (int j = i; j <= (2 * i) - 1; j++)//Loop for first half of the triangle
                         Console.Write("*");
-                    for (int j = 0; j < i - 1; j++)
+                    for (int j = 0; j < i - 1; j++)//Loop for the second half of the triangle
                         Console.Write("*");
-                    Console.WriteLine(" \n");
+                    Console.WriteLine(" \n");//For getting the stars in the next line after the loop
                 }
             }
             catch
@@ -117,7 +117,18 @@ namespace Assignment1_F19
         {
             try
             {
-                // Write your code here
+                int common  = 0;
+                for (int a = 0; a < J.Length; a++)//Taking every element from the first array
+                {
+                    for (int b = 0; b < S.Length; b++)//Taking every element from the second array
+                    {
+                        if (J[a] == S[b]) //Comparing the elements from both the arrays
+                        {
+                            common += 1; //Getting the count of such matched values
+                        }
+                    }
+                }
+                return common; //Getting the outpiut
             }
             catch
             {
